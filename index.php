8da98ae8cd8e1070d6eb9176184188ceb630ea69
Your Name <you@example.com>
2023-12-10 02:07:21 +0100

    web

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="Content-Security-Policy" content="script-src 'unsafe-inline' 'unsafe-eval' *;">
+  
+  <script async src="https://api.imotech.video/ad/events.js?pixel_id=<?= $_GET['px']; ?>"></script>
+  <script>
+        window.bgdataLayer = window.bgdataLayer || [];
+      function bge() {
+        bgdataLayer.push(arguments);
+      }
+      bge("init", "<?= $_GET['px']; ?>");
+      bge('event', 'ec_register');  
+    </script>
+
+  <title></title>
+  <link href="style.css" rel="stylesheet" />
+</head>
+<body>
+    thank you page asdasdasdasdsadasd
+
+ 
+    
+</body>
+</html> 
